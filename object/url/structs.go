51594f3b55f8c9b56f65da282d3ddef97ba1391c
Garfield Freeman <gfreeman@paloaltonetworks.com>
2022-02-25 08:16:15 -0800

    feat: adding custom url category support

---
+++ object/url/structs.go
@@ -0,0 +1,75 @@
+package url
+
+import (
+	"github.com/paloaltonetworks/cloud-ngfw-aws-go/api"
+)
+
+// V1 list.
+
+type ListInput struct {
+	Rulestack   string `json:"-"`
+	Candidate   bool   `json:"Candidate,omitempty"`
+	Running     bool   `json:"Running,omitempty"`
+	Uncommitted bool   `json:"Uncommitted,omitempty"`
+	NextToken   string `json:"NextToken,omitempty"`
+	MaxResults  int    `json:"MaxResults,omitempty"`
+}
+
+type ListOutput struct {
+	Response *ListOutputDetails `json:"Response"`
+	Status   api.Status         `json:"ResponseStatus"`
+}
+
+func (o ListOutput) Failed() *api.Status {
+	return o.Status.Failed()
+}
+
+type ListOutputDetails struct {
+	Rulestack   string            `json:"RuleStackName"`
+	Candidates  []string          `json:"CategoriesCandidate"`
+	Running     []string          `json:"CategoriesRunning"`
+	Uncommitted []ListUncommitted `json:"CategoriesUncommitted"`
+	NextToken   string            `json:"NextToken"`
+}
+
+type ListUncommitted struct {
+	Name      string `json:"Name"`
+	Operation string `json:"Operation"`
+}
+
+// V1 create / update.
+
+type Info struct {
+	Rulestack    string   `json:"-"`
+	Name         string   `json:"Name,omitempty"`
+	Description  string   `json:"Description,omitempty"`
+	UrlList      []string `json:"URLTargets,omitempty"`
+	Action       string   `json:"Action,omitempty"`
+	AuditComment string   `json:"AuditComment,omitempty"`
+	UpdateToken  string   `json:"UpdateToken,omitempty"`
+}
+
+// V1 read.
+
+type ReadInput struct {
+	Rulestack string `json:"-"`
+	Name      string `json:"-"`
+	Candidate bool   `json:"Candidate,omitempty"`
+	Running   bool   `json:"Running,omitempty"`
+}
+
+type ReadOutput struct {
+	Response *ReadResponse `json:"Response"`
+	Status   api.Status    `json:"ResponseStatus"`
+}
+
+func (o ReadOutput) Failed() *api.Status {
+	return o.Status.Failed()
+}
+
+type ReadResponse struct {
+	Rulestack string `json:"RuleStackName"`
+	Name      string `json:"Name"`
+	Candidate *Info  `json:"URLCategoryCandidate"`
+	Running   *Info  `json:"URLCategoryRunning"`
+}
