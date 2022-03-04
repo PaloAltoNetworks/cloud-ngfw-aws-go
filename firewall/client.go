afc2a2826a4bbbfa1ed91115ac2e4c5406600935
Migara Ekanayake <2110772+migara@users.noreply.github.com>
2022-03-04 07:49:47 +0000

    add firewall CRUDL

---
+++ firewall/client.go
@@ -0,0 +1,157 @@
+package firewall
+
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	"github.com/paloaltonetworks/cloud-ngfw-aws-go/api"
+	"github.com/paloaltonetworks/cloud-ngfw-aws-go/permissions"
+)
+
+// Client is a client for this collection.
+type Client struct {
+	client api.Client
+}
+
+// NewClient returns a new client for this collection.
+func NewClient(client api.Client) *Client {
+	return &Client{client: client}
+}
+
+// List returns a list of firewalls.
+func (c *Client) List(ctx context.Context, input ListInput) (ListOutput, error) {
+	if len(input.VpcIds) == 0 {
+		c.client.Log(http.MethodGet, "list NGFirewalls in all the VPCs")
+	} else {
+		c.client.Log(http.MethodGet, "list NGFirewalls in %q VPCs", strings.Join(input.VpcIds, ","))
+	}
+
+	var ans ListOutput
+	_, err := c.client.Communicate(
+		ctx,
+		permissions.Firewall,
+		http.MethodGet,
+		[]string{"v1", "config", "ngfirewalls"},
+		nil,
+		input,
+		&ans,
+	)
+
+	return ans, err
+}
+
+// Create creates an object.
+func (c *Client) Create(ctx context.Context, input Info) error {
+	c.client.Log(http.MethodPost, "create firewall %q", input.Name)
+
+	_, err := c.client.Communicate(
+		ctx,
+		permissions.Firewall,
+		http.MethodPost,
+		[]string{"v1", "config", "ngfirewalls"},
+		nil,
+		input,
+		nil,
+	)
+
+	return err
+}
+
+// Read returns information on the given object.
+func (c *Client) Read(ctx context.Context, input ReadInput) (ReadOutput, error) {
+	name := input.Name
+	c.client.Log(http.MethodGet, "describe firewall: %s", name)
+
+	var ans ReadOutput
+	_, err := c.client.Communicate(
+		ctx,
+		permissions.Rulestack,
+		http.MethodGet,
+		[]string{"v1", "config", "ngfirewalls", name},
+		nil,
+		input,
+		&ans,
+	)
+
+	return ans, err
+}
+
+// UpdateDescription updates the description of the firewall.
+func (c *Client) UpdateDescription(ctx context.Context, input Info) error {
+	name := input.Name
+	input.Name = ""
+
+	c.client.Log(http.MethodPut, "updating firewall %q description: %s", name, input.Description)
+
+	_, err := c.client.Communicate(
+		ctx,
+		permissions.Firewall,
+		http.MethodPut,
+		[]string{"v1", "config", "ngfirewalls", name, "description"},
+		nil,
+		input,
+		nil,
+	)
+
+	return err
+}
+
+// UpdateNGFirewallContentVersion updates the content version of the firewall.
+func (c *Client) UpdateNGFirewallContentVersion(ctx context.Context, input Info) error {
+	name := input.Name
+	input.Name = ""
+
+	c.client.Log(http.MethodPut, "updating firewall %q content version: %s", name, input.AppIdVersion)
+
+	_, err := c.client.Communicate(
+		ctx,
+		permissions.Firewall,
+		http.MethodPut,
+		[]string{"v1", "config", "ngfirewalls", name, "contentversion"},
+		nil,
+		input,
+		nil,
+	)
+
+	return err
+}
+
+// UpdateSubnetMappings updates the subnet mappings of the firewall.
+func (c *Client) UpdateSubnetMappings(ctx context.Context, input Info) error {
+	name := input.Name
+	input.Name = ""
+
+	c.client.Log(http.MethodPut, "updating firewall %q associate subnet mappings: %s disassociate subnet mappings: %s", name, input.AssociateSubnetMappings, input.DisassociateSubnetMappings)
+
+	_, err := c.client.Communicate(
+		ctx,
+		permissions.Firewall,
+		http.MethodPut,
+		[]string{"v1", "config", "ngfirewalls", name, "subnets"},
+		nil,
+		input,
+		nil,
+	)
+
+	return err
+}
+
+// Delete the given firewall.
+func (c *Client) Delete(ctx context.Context, input ReadInput) error {
+	name := input.Name
+
+	c.client.Log(http.MethodDelete, "delete firewall: %s", name)
+
+	_, err := c.client.Communicate(
+		ctx,
+		permissions.Firewall,
+		http.MethodDelete,
+		[]string{"v1", "config", "ngfirewalls", name},
+		nil,
+		input,
+		nil,
+	)
+
+	return err
+}
---
+++ firewall/structs.go
@@ -0,0 +1,115 @@
+package firewall
+
+import (
+	"github.com/paloaltonetworks/cloud-ngfw-aws-go/api"
+)
+
+// V1 list.
+
+type ListInput struct {
+	MaxResults int      `json:"MaxResults,omitempty"`
+	NextToken  string   `json:"NextToken,omitempty"`
+	VpcIds     []string `json:"VpcIds,omitempty"`
+}
+
+type ListOutput struct {
+	Response *ListOutputDetails `json:"Response"`
+	Status   api.Status         `json:"ResponseStatus"`
+}
+
+func (o ListOutput) Failed() *api.Status {
+	return o.Status.Failed()
+}
+
+type ListOutputDetails struct {
+	Firewalls []ListFirewall `json:"Firewalls"`
+	NextToken string         `json:"NextToken"`
+}
+
+type ListFirewall struct {
+	Name      string `json:"FirewallName"`
+	AccountId string `json:"AccountId"`
+}
+
+// V1 create / update.
+
+type Info struct {
+	Name                         string          `json:"FirewallName,omitempty"`
+	VpcId                        string          `json:"VpcId,omitempty"`
+	AccountId                    string          `json:"AccountId,omitempty"`
+	Description                  string          `json:"Description,omitempty"`
+	EndpointMode                 string          `json:"EndpointMode,omitempty"`
+	SubnetMappings               []SubnetMapping `json:"SubnetMappings,omitempty"`
+	AppIdVersion                 string          `json:"AppIdVersion,omitempty"`
+	AutomaticUpgradeAppIdVersion bool            `json:"AutomaticUpgradeAppIdVersion,omitempty"`
+	RuleStackName                string          `json:"RuleStackName,omitempty"`
+	GlobalRuleStackName          string          `json:"GlobalRuleStackName,omitempty"`
+	Tags                         []TagDetails    `json:"Tags,omitempty"`
+
+	AssociateSubnetMappings    []SubnetMapping `json:"AssociateSubnetMappings,omitempty"`
+	DisassociateSubnetMappings []SubnetMapping `json:"DisassociateSubnetMappings,omitempty"`
+
+	UpdateToken string `json:"UpdateToken,omitempty"`
+}
+
+type SubnetMapping struct {
+	SubnetId           string `json:"SubnetId,omitempty"`
+	AvailabilityZone   string `json:"AvailabilityZone,omitempty"`
+	AvailabilityZoneId string `json:"AvailabilityZoneId,omitempty"`
+}
+
+type TagDetails struct {
+	Key   string `json:"Key"`
+	Value string `json:"Value"`
+}
+
+// V1 read.
+
+type ReadInput struct {
+	Name      string `json:"-"`
+	AccountId string `json:"AccountId,omitempty"`
+}
+
+type ReadOutput struct {
+	Response *ReadResponse `json:"Response"`
+	Status   api.Status    `json:"ResponseStatus"`
+}
+
+func (o ReadOutput) Failed() *api.Status {
+	return o.Status.Failed()
+}
+
+type ReadResponse struct {
+	Firewall Firewall       `json:"Firewall,omitempty"`
+	Status   FirewallStatus `json:"Status,omitempty"`
+}
+
+type Firewall struct {
+	Name                         string          `json:"FirewallName,omitempty"`
+	AccountId                    string          `json:"AccountId,omitempty"`
+	SubnetMappings               []SubnetMapping `json:"SubnetMappings,omitempty"`
+	VpcId                        string          `json:"VpcId,omitempty"`
+	AppIdVersion                 string          `json:"AppIdVersion,omitempty"`
+	Description                  string          `json:"Description,omitempty"`
+	RuleStackName                string          `json:"RuleStackName,omitempty"`
+	GlobalRuleStackName          string          `json:"GlobalRuleStackName,omitempty"`
+	EndpointServiceName          string          `json:"EndpointServiceName,omitempty"`
+	EndpointMode                 string          `json:"EndpointMode,omitempty"`
+	AutomaticUpgradeAppIdVersion bool            `json:"AutomaticUpgradeAppIdVersion,omitempty"`
+	Tags                         []TagDetails    `json:"Tags,omitempty"`
+	UpdateToken                  string          `json:"UpdateToken,omitempty"`
+}
+
+type FirewallStatus struct {
+	FirewallStatus  string       `json:"FirewallStatus,omitempty"`
+	FailureReason   string       `json:"FailureReason,omitempty"`
+	RuleStackStatus string       `json:"RuleStackStatus,omitempty"`
+	Attachments     []Attachment `json:"Attachments,omitempty"`
+}
+
+type Attachment struct {
+	EndpointId     string `json:"EndpointId,omitempty"`
+	Status         string `json:"Status,omitempty"`
+	RejectedReason string `json:"RejectedReason,omitempty"`
+	SubnetId       string `json:"SubnetId,omitempty"`
+}
